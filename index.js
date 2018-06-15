d3a968d35aeee91aac91e863e3ff64dd2b165873
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-06-15 21:39:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo(you) {
-  return `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`;
 }
 function happyHolidaysTo(holiday, days) {
-  return `Happy ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
